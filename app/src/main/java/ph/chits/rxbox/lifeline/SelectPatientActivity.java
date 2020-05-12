beac16c423007693861170ad27d970717de421ef
calvin <rupcal@gmail.com>
2020-05-12 11:54:54 +0800

    refactored ui control

--- app/src/main/java/ph/chits/rxbox/lifeline/SelectPatientActivity.java
+++ app/src/main/java/ph/chits/rxbox/lifeline/SelectPatientActivity.java
@@ -36,6 +36,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class SelectPatientActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName();
 
+    private enum State {
+        RESET,
+        READING_PATIENT_QUEUE,
+        READING_PATIENT_QUEUE_NOT_OK,
+        READING_PATIENT_QUEUE_INVALID_BODY,
+        READING_PATIENT_QUEUE_ERROR,
+        EMPTY_PATIENT_QUEUE,
+        READING_PATIENT_DETAILS,
+        READING_PATIENT_DETAILS_NOT_OK,
+        READING_PATIENT_DETAILS_INVALID_BODY,
+        READING_PATIENT_DETAILS_ERROR,
+        READING_PATIENT_DETAILS_SUCCESS
+    }
+
     private List<Patient> patients = new ArrayList<>();
     private List<Patient> filteredPatients = new ArrayList<>();
     PatientAdapter patientAdapter;
@@ -43,6 +57,7 @@ public class SelectPatientActivity extends AppCompatActivity {
     private Call<PatientQueue> patientQueueCall;
     private List<Call<ph.chits.rxbox.lifeline.rest.Patient>> patientDetailsCalls;
     CountDownLatch latch;
+    List<String> patientIds;
 
     private ProgressBar progressBar;
     private TextView textMessage;
@@ -51,11 +66,6 @@ public class SelectPatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_patient);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
 
         RecyclerView recycler;
         recycler = findViewById(R.id.list_patients);
@@ -139,6 +149,11 @@ public class SelectPatientActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         fhirService = retrofit.create(FhirService.class);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         readPatientQueue();
     }
@@ -154,16 +169,7 @@ public class SelectPatientActivity extends AppCompatActivity {
             }
         }
 
-        // clear the buffers
-        patients.clear();
-        filteredPatients.clear();
-
-        // update ui
-        patientAdapter.notifyDataSetChanged();
-        patientAdapter.clearSelected();
-        progressBar.setIndeterminate(true);
-        progressBar.setVisibility(View.VISIBLE);
-        textMessage.setVisibility(View.GONE);
+        updateUi(State.RESET, State.READING_PATIENT_QUEUE);
 
         // make the call
         patientQueueCall = fhirService.readPatientQueue();
@@ -177,85 +183,64 @@ public class SelectPatientActivity extends AppCompatActivity {
                     if (response.body() != null) {
                         readPatientDetails(response.body());
                     } else {
-                        textMessage.setText(R.string.patient_queue_error);
-                        textMessage.setVisibility(View.VISIBLE);
-                        progressBar.setVisibility(View.GONE);
+                        updateUi(State.READING_PATIENT_QUEUE_INVALID_BODY);
                     }
                 } else {
-                    textMessage.setText(R.string.patient_queue_error);
-                    textMessage.setVisibility(View.VISIBLE);
-                    progressBar.setVisibility(View.GONE);
+                    updateUi(State.READING_PATIENT_QUEUE_NOT_OK);
                 }
-
-
             }
 
             @Override
             public void onFailure(Call<PatientQueue> call, Throwable t) {
+                updateUi(State.READING_PATIENT_QUEUE_ERROR);
                 Log.d(TAG, "failure", t);
             }
         });
     }
 
     private void readPatientDetails(PatientQueue queue) {
-        final List<String> ids = queue.getPatientIds();
-        if (ids.size() <= 0) {
-            textMessage.setText(R.string.patient_queue_empty);
-            textMessage.setVisibility(View.VISIBLE);
-            progressBar.setProgress(100);
-            progressBar.setIndeterminate(false);
-            progressBar.setVisibility(View.GONE);
+        patientIds = queue.getPatientIds();
+        if (patientIds.size() <= 0) {
+            updateUi(State.EMPTY_PATIENT_QUEUE);
         } else {
-            progressBar.setProgress(0);
-            progressBar.setIndeterminate(false);
+            latch = new CountDownLatch(patientIds.size());
+            updateUi(State.READING_PATIENT_DETAILS);
 
-            latch = new CountDownLatch(ids.size());
-
-            patientDetailsCalls = new ArrayList<>(ids.size());
-            for (String id : ids) {
+            patientDetailsCalls = new ArrayList<>(patientIds.size());
+            for (String id : patientIds) {
                 Call<ph.chits.rxbox.lifeline.rest.Patient> patientCall = fhirService.readPatient(id);
                 patientDetailsCalls.add(patientCall);
                 patientCall.enqueue(new Callback<ph.chits.rxbox.lifeline.rest.Patient>() {
                     @Override
                     public void onResponse(Call<ph.chits.rxbox.lifeline.rest.Patient> call, Response<ph.chits.rxbox.lifeline.rest.Patient> response) {
-                        Log.d(TAG, "patient " + response.toString());
-                        if (response.body() != null) {
-                            Patient patient = new Patient(response.body().getId(),
-                                    response.body().getName(),
-                                    response.body().getGender(),
-                                    response.body().getBirthdate()
-                            );
-                            patients.add(patient);
-                            filteredPatients.add(patient);
-                            patientAdapter.notifyItemInserted(filteredPatients.size() - 1);
+                        if (response.code() == 200) {
+                            if (response.body() != null) {
+                                Patient patient = new Patient(response.body().getId(),
+                                        response.body().getName(),
+                                        response.body().getGender(),
+                                        response.body().getBirthdate()
+                                );
+                                patients.add(patient);
+                                //filteredPatients.add(patient);
+                                //patientAdapter.notifyItemInserted(filteredPatients.size() - 1);
 
-                            latch.countDown();
-                            int prog = Math.round(100 * (1 - (float) latch.getCount() / ids.size()));
-                            progressBar.setProgress(prog);
-                            Log.d(TAG, "progress " + prog);
-                            if (latch.getCount() == 0) {
-                                progressBar.setVisibility(View.GONE);
+                                latch.countDown();
+                                updateUi(State.READING_PATIENT_DETAILS);
+                            } else {
+                                updateUi(State.READING_PATIENT_DETAILS_INVALID_BODY);
+
+                                for (Call<ph.chits.rxbox.lifeline.rest.Patient> patientCall : patientDetailsCalls) {
+                                    patientCall.cancel();
+                                }
                             }
                         } else {
-                            patients.clear();
-                            filteredPatients.clear();
-                            patientAdapter.notifyDataSetChanged();
-                            textMessage.setText(R.string.patient_queue_error);
-                            textMessage.setVisibility(View.VISIBLE);
-
-                            for (Call<ph.chits.rxbox.lifeline.rest.Patient> patientCall : patientDetailsCalls) {
-                                patientCall.cancel();
-                            }
+                            updateUi(State.READING_PATIENT_DETAILS_NOT_OK);
                         }
                     }
 
                     @Override
                     public void onFailure(Call<ph.chits.rxbox.lifeline.rest.Patient> call, Throwable t) {
-                        patients.clear();
-                        filteredPatients.clear();
-                        patientAdapter.notifyDataSetChanged();
-                        textMessage.setText(R.string.patient_queue_error);
-                        textMessage.setVisibility(View.VISIBLE);
+                        updateUi(State.READING_PATIENT_DETAILS_ERROR);
                     }
                 });
             }
@@ -275,5 +260,63 @@ public class SelectPatientActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void updateUi(State... states) {
+        for (State s : states) {
+            updateUi(s);
+        }
+    }
+
+    private void updateUi(State state) {
+        switch (state) {
+            case RESET:
+                patients.clear();
+                filteredPatients.clear();
+                patientAdapter.notifyDataSetChanged();
+                patientAdapter.clearSelected();
+                progressBar.setIndeterminate(true);
+                break;
+            case READING_PATIENT_QUEUE:
+                progressBar.setVisibility(View.VISIBLE);
+                textMessage.setVisibility(View.GONE);
+                break;
+            case READING_PATIENT_DETAILS_INVALID_BODY:
+            case READING_PATIENT_DETAILS_NOT_OK:
+            case READING_PATIENT_DETAILS_ERROR:
+                textMessage.setText(R.string.patient_queue_error);
+                textMessage.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+                break;
+            case EMPTY_PATIENT_QUEUE:
+                textMessage.setText(R.string.patient_queue_empty);
+                textMessage.setVisibility(View.VISIBLE);
+                progressBar.setProgress(100);
+                progressBar.setIndeterminate(false);
+                progressBar.setVisibility(View.GONE);
+                break;
+            case READING_PATIENT_DETAILS:
+                progressBar.setIndeterminate(false);
+                int progress = Math.round(100 * (1 - (float) latch.getCount() / patientIds.size()));
+                progressBar.setProgress(progress);
+                Log.d(TAG, "progress " + progress);
+                if (latch.getCount() == 0) {
+                    progressBar.setVisibility(View.GONE);
+                    updateUi(State.READING_PATIENT_DETAILS_SUCCESS);
+                }
+                break;
+            case READING_PATIENT_QUEUE_INVALID_BODY:
+            case READING_PATIENT_QUEUE_NOT_OK:
+            case READING_PATIENT_QUEUE_ERROR:
+                patients.clear();
+                filteredPatients.clear();
+                patientAdapter.notifyDataSetChanged();
+                textMessage.setText(R.string.patient_queue_error);
+                textMessage.setVisibility(View.VISIBLE);
+                break;
+            case READING_PATIENT_DETAILS_SUCCESS:
+                filteredPatients.addAll(patients);
+                patientAdapter.notifyDataSetChanged();
+        }
     }
 }
